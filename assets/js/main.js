23db29ed0406b58740947905ad895ffd4f19ffc3
Jule23sc <vrauvausvl21@gmail.com>
2023-07-03 12:42:49 +0200

    difference

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 "use strict";
-
+const output = document.querySelector(".output");
 function calculateAge(event) {
     event.preventDefault();
     const ageOne = document.getElementById("ageone").value;
     const ageTwo = document.getElementById("agetwo").value;
-    calculateAge.innerHTML = ageOne-ageTwo;
+    output.innerHTML = ageOne-ageTwo;
 }
-console.log(calculateAge)
